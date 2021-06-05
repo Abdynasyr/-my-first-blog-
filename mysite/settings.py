31c618b2d9a9efefe8e1290ec926ffd586214826
Abdynasyr <abdynasyr@gmail.com>
2021-06-06 00:13:29 +0600

    My Django Girls app, first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,8 +12,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.abspath((os.path.join(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'blog',
 ]
 
 MIDDLEWARE = [
